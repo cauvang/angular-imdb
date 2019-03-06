e5ddab6807e59f2d3d566cba8536254a60ef818d
cauvang <cauvang@yahoo.com>
2019-03-06 23:56:02 +1100

    update news links

--- src/app/homepage/news/news.component.ts
+++ src/app/homepage/news/news.component.ts
@@ -10,14 +10,6 @@ import { ISource, INews } from 'src/app/models/news';
 export class NewsComponent implements OnInit {
   @Input() data: INews[];
   @Input() topData: INews;
-  @Input() date: Date;
-  @Input() auhor: string;
-  @Input() title: string;
-  @Input() url: string;
-  // @Input() id: string;
-  @Input() content: string;
-  @Input() image: IImage;
-  @Input() source: ISource;
 
   constructor() { }
 
