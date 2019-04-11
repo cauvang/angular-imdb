bb80d063610f075946612ac1871883d52d17cfdc
Truong Nguyen <truong.nguyen@team.telstra.com>
2019-04-11 23:24:46 +1000

    title home menu

--- src/app/titles/titles-home/titles-home.component.ts
+++ src/app/titles/titles-home/titles-home.component.ts
@@ -1,16 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-@Component({
-  selector: 'app-titles-home',
-  templateUrl: './titles-home.component.html',
-  styleUrls: ['./titles-home.component.scss']
-})
+@Component({selector: 'app-titles-home', templateUrl: './titles-home.component.html', styleUrls: ['./titles-home.component.scss']})
 export class TitlesHomeComponent implements OnInit {
-  private headers: string[] = ['Full cast and crew', 'Trivia', 'User reviews'];
+  private headers : string[] = ['Full cast and crew', 'Trivia', 'User reviews'];
+  public toggleMegaMenu = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  toggleMenuClick() {
+    this.toggleMegaMenu = !this.toggleMegaMenu;
   }
-
 }
