1946412e667fc948441b57000506fb154b96cd10
Truong Nguyen <truong.nguyen@team.telstra.com>
2019-04-13 23:03:51 +1000

    style update

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,16 +1,33 @@
-import { Directive, HostListener, HostBinding } from '@angular/core';
+import {Directive, HostListener, HostBinding, ElementRef, Renderer2} from '@angular/core';
+import {Router, Event, NavigationEnd} from '@angular/router';
 
-@Directive({
-    selector: '[appDropdown]',
-    exportAs: 'appDropDown'
-})
+@Directive({selector: '[appDropdown]', exportAs: 'appDropDown'})
 export class DropdownDirective {
-    @HostBinding('class.show') isOpen = false;
+  constructor(private el : ElementRef, private router : Router, private renderer : Renderer2) {
+    this
+      .router
+      .events
+      .subscribe((event : Event) => {
+        if (event instanceof NavigationEnd) {
 
-    @HostListener('click') toggleOpen() {
-        this.isOpen = !this.isOpen;
-    }
+          this.isOpen = false;
+          this
+            .renderer
+            .removeClass(this.el.nativeElement, 'show');
+        }
+      });
+  }
+  @HostBinding('class.show')isOpen = false;
 
-
+  @HostListener('click')toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+  @HostListener('mouseover')mouseOver() {
+    this.isOpen = true;
+  }
+  @HostListener('mouseout')mouseOut() {
+    this.isOpen = false;
+  }
 
 }
+ 
