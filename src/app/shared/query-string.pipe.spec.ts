25a828d386fde44274f8329ec3c468a0b3ae4d27
Truong Nguyen <truong.nguyen@team.telstra.com>
2019-04-15 14:15:37 +1000

    fix url encoded issuer with routerLink

---
+++ src/app/shared/query-string.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { QueryStringPipe } from './query-string.pipe';
+
+describe('QueryStringPipe', () => {
+  it('create an instance', () => {
+    const pipe = new QueryStringPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
